0ea828e458a2353ab7ba70049d0225baec9c8621
Jhaman1895 <ajha052316@gmail.com>
2022-10-09 13:08:23 +0530

    Finish adding the rest of the game logic.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const randomNumber = Math.trunc(Math.random()*20) + 1;
+
+let initialScore = 20;
+
+document.querySelector('.number').textContent = randomNumber;
+
 document.querySelector('.check').addEventListener('click',
 function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -8,4 +14,30 @@ function () {
     if(!guess) {
         document.querySelector('.message').textContent = 'Not a valid Number!';
     }
+    else if(guess == randomNumber) {
+        document.querySelector('.message').textContent = 'Correct!';
+        initialScore++;
+        document.querySelector('.score').textContent = initialScore;
+    }
+    else if (guess > randomNumber) {
+        if (initialScore > 1) {
+        document.querySelector('.message').textContent = 'Number is too high!';
+        initialScore--;
+        document.querySelector('.score').textContent = initialScore;
+        } else {
+            document.querySelector('.message').textContent = 'You have Lost...';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+    else if (guess < randomNumber) {
+        if (initialScore > 1) {
+            document.querySelector('.message').textContent = 'Number is too low!';
+            initialScore--;
+            document.querySelector('.score').textContent = initialScore;
+            } else {
+                document.querySelector('.message').textContent = 'You have Lost...';
+                document.querySelector('.score').textContent = 0;
+            }
+    };
 });
+
